285b6a70c631e9e18d9e1b6967fc53e8074ff91c
gars150387 <gar.santeliz@gmail.com>
2021-07-23 18:40:43 +0000

    added components

---
+++ src/js/views/Contact.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import { Card } from "../component/Card.js";
+import { Modal } from "../component/Modal";
+
+export const Contact = () => {
+	const [state, setState] = useState({
+		showModal: false
+	});
+
+	return (
+		<div className="container">
+			<div>
+				<p className="text-right my-3">
+					<Link className="btn btn-success" to="/add">
+						Add new contact
+					</Link>
+				</p>
+				<div id="contact" className="panel-collapse collapse show" aria-expanded="true">
+					<ul className="list-group pull-down" id="contact-list">
+						<Card onDelete={() => setState({ showModal: true })} />
+						<Card />
+						<Card />
+						<Card />
+					</ul>
+				</div>
+			</div>
+			<Modal show={state.showModal} onClose={() => setState({ showModal: false })} />
+		</div>
+	);
+};
